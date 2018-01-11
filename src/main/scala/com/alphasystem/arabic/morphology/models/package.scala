09119653945aa6aeca84c2dca334f9ba67d84493
sfali23 <f.syed.ali@gmail.com>
2018-01-10 20:26:27 -0500

    changed RootLetters to have index as well and RootWord class

--- src/main/scala/com/alphasystem/arabic/morphology/models/package.scala
+++ src/main/scala/com/alphasystem/arabic/morphology/models/package.scala
@@ -1,7 +1,8 @@
 package com.alphasystem.arabic.morphology
 
-import com.alphasystem.arabic.model.ArabicLetterType
-import com.alphasystem.arabic.model.ArabicLetterType.{AIN, FA, LAM}
+import com.alphasystem.arabic.model.{ ArabicLetterType, ArabicSupport, ArabicWord, SarfMemberType }
+import com.alphasystem.arabic.model.ArabicLetterType.{ AIN, FA, LAM }
+import com.alphasystem.arabic.morphology.model.support.SarfTermType
 
 /**
   * @author sali
@@ -16,18 +17,30 @@ package object models {
     val displayName: String
   }
 
-  case class RootLetters(firstRadical: ArabicLetterType = FA,
-                         secondRadical: ArabicLetterType = AIN,
-                         thirdRadical: ArabicLetterType = LAM,
-                         fourthRadical: Option[ArabicLetterType] = None)
-    extends AbstractSimpleDocument {
+  case class Radical(letter: ArabicLetterType, index: Int)
+
+  case class RootLetters(firstRadical: Radical = Radical(FA, 0),
+                         secondRadical: Radical = Radical(AIN, 1),
+                         thirdRadical: Radical = Radical(LAM, 2),
+                         fourthRadical: Option[Radical] = None)
+      extends AbstractSimpleDocument {
 
     override val id: String = {
       val fr = fourthRadical match {
         case None => ""
-        case Some(value) => s"_${value.name()}"
+        case Some(value) => s"_${value.letter.name()}"
       }
-      s"${firstRadical.name()}_${secondRadical.name()}_${thirdRadical.name()}$fr"
+      s"${firstRadical.letter.name()}_${secondRadical.letter.name()}_${thirdRadical.letter.name()}$fr"
     }
+  }
+
+  case class RootWord(rootLetters: RootLetters,
+                      sarfTermType: SarfTermType,
+                      sarfMemberType: SarfMemberType,
+                      baseWord: ArabicWord,
+                      rootWord: ArabicWord)
+      extends ArabicSupport {
+
+    override def toLabel: ArabicWord = rootWord
   }
 }
